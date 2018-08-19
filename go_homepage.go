72a8b8dd586544be613525cdebec055f41a29b94
Steve Chung <steve.chung7@gmail.com>
2018-08-19 11:55:18 -0400

    separate go_homepage.go code to cmd
    make go_homepage.go just have config related stuff

--- go_homepage.go
+++ go_homepage.go
@@ -1,28 +1,18 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-	"time"
-
 	"github.com/sirupsen/logrus"
+	"os"
 
 	"github.com/s12chung/go_homepage/go/app"
+	"github.com/s12chung/go_homepage/go/cmd"
 	"github.com/s12chung/go_homepage/go/content/models"
 	"github.com/s12chung/go_homepage/go/content/routes"
 	"github.com/s12chung/go_homepage/go/lib/html"
-	"github.com/s12chung/go_homepage/go/lib/router"
 )
 
 func main() {
-	var log logrus.FieldLogger
-	start := time.Now()
-	defer func() {
-		log.Infof("Build completed in %v.", time.Now().Sub(start))
-	}()
-
-	log = &logrus.Logger{
+	log := &logrus.Logger{
 		Out: os.Stderr,
 		Formatter: &logrus.TextFormatter{
 			ForceColors: true,
@@ -36,31 +26,13 @@ func main() {
 		"type": "models",
 	}))
 
-	err := run(s, log)
+	err := cmd.NewCmd(s, func() app.Setter {
+		renderer := html.NewRenderer(s.GeneratedPath, &s.Template, log)
+		respondHelper := app.NewRespondHelper(renderer, s)
+		return routes.NewSetter(respondHelper)
+	}).Run(log)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
-	}
-}
-
-func run(s *app.Settings, log logrus.FieldLogger) error {
-	fileServerPtr := flag.Bool("file-server", false, fmt.Sprintf("Serves, but not generates, generated files in %v on localhost:%v", s.GeneratedPath, s.FileServerPort))
-	serverPtr := flag.Bool("server", false, fmt.Sprintf("Hosts server on localhost:%v", s.ServerPort))
-
-	flag.Parse()
-
-	if *fileServerPtr {
-		return router.RunFileServer(s.GeneratedPath, s.FileServerPort, log)
-	} else {
-		renderer := html.NewRenderer(s.GeneratedPath, &s.Template, log)
-		respondHelper := app.NewRespondHelper(renderer, s)
-		routeSetter := routes.NewSetter(respondHelper)
-		a := app.NewApp(routeSetter, s, log)
-
-		if *serverPtr {
-			return a.Host()
-		} else {
-			return a.Generate()
-		}
 	}
 }
