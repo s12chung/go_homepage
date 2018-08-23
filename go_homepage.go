4ecb7046acc8de509e6deccd071c53a0797ed445
Steve Chung <steve.chung7@gmail.com>
2018-08-23 00:17:43 -0400

    make go_homepage explicity set settings
    think it's better now...

--- go_homepage.go
+++ go_homepage.go
@@ -6,13 +6,16 @@ import (
 	"github.com/s12chung/go_homepage/go/content"
 	"github.com/s12chung/gostatic/go/app"
 	"github.com/s12chung/gostatic/go/cli"
+	"github.com/s12chung/gostatic/go/lib/utils"
 )
 
 func main() {
 	log := app.DefaultLog()
 
+	settings := app.DefaultSettings()
 	contentSettings := content.DefaultSettings()
-	settings := app.SettingsFromFile("./settings.json", contentSettings, log)
+	settings.Content = contentSettings
+	utils.SettingsFromFile("./settings.json", settings, log)
 
 	theContent := content.NewContent(settings.GeneratedPath, contentSettings, log)
 	err := cli.Run(app.NewApp(theContent, settings, log))
