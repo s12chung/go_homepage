5a4883eabbf7ed1b2800249f652e7650eaf1e821
Steve Chung <steve.chung7@gmail.com>
2018-08-28 17:29:26 -0400

    update webpack code to match gostatic blueprint

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
 const isProduction = process.env.NODE_ENV === "production";
-const filenameF = function() { return isProduction ? '[name]-[hash]' : '[name]'; };
+const filename = isProduction ? '[name]-[hash]' : '[name]';
 
-const defaults = require('gostatic-webpack')(__dirname, filenameF);
-const imageGenerator = require('gostatic-webpack-images')(__dirname, filenameF);
+const defaults = require('gostatic-webpack')(__dirname, filename, isProduction);
+const imageDefaults = require('gostatic-webpack-images')(__dirname, filename, isProduction);
 
 const relativePath = function(p) { return require('path').resolve(__dirname, p); };
 
 module.exports = {
     mode: isProduction ? "production" : "development",
+    devtool: isProduction ? undefined : "eval",
 
     entry: Object.assign(defaults.entry(), {
+        // entryChunkName: relativePath('assets/js/filename.js'),
     }),
     output: Object.assign(defaults.output(), {
+        // customize
     }),
 
     module: {
-        rules: defaults.allRules().concat(
-            imageGenerator.responsiveRules(relativePath('content'), 'content/images/')
-        )
+        rules: defaults.allRules()
+            .concat(imageDefaults.responsiveRules(relativePath('content'), 'content/images/'))
     },
 
-    plugins: defaults.allPlugins().concat([
-    ])
+    plugins: defaults.allPlugins()
+        .concat([
+            // customize
+        ])
 };
